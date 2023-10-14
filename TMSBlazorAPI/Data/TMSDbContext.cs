7b4aa105ce450274288296801c039c9c63e1a5a2
mikrewongel <wongelmikre@gmail.com>
2023-10-14 16:02:45 -0400

    Add project files.

---
+++ TMSBlazorAPI/Data/TMSDbContext.cs
@@ -0,0 +1,76 @@
+﻿using System;
+using System.Collections.Generic;
+using Microsoft.EntityFrameworkCore;
+
+namespace TMSBlazorAPI.Data;
+
+public partial class TMSDbContext : DbContext
+{
+    public TMSDbContext()
+    {
+    }
+
+    public TMSDbContext(DbContextOptions<TMSDbContext> options)
+        : base(options)
+    {
+    }
+
+    public virtual DbSet<Club> Clubs { get; set; }
+
+    public virtual DbSet<User> Users { get; set; }
+
+    protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        => optionsBuilder.UseSqlServer("name=TMSconn");
+
+    protected override void OnModelCreating(ModelBuilder modelBuilder)
+    {
+        modelBuilder.Entity<Club>(entity =>
+        {
+            entity.HasKey(e => e.ClubId).HasName("PK__Club__D35058C732832EBF");
+
+            entity.ToTable("Club");
+
+            entity.Property(e => e.ClubId).HasColumnName("ClubID");
+            entity.Property(e => e.ClubName)
+                .HasMaxLength(250)
+                .IsUnicode(false);
+            entity.Property(e => e.CreatedDate)
+                .HasDefaultValueSql("(getdate())")
+                .HasColumnType("datetime");
+            entity.Property(e => e.UserId).HasColumnName("UserID");
+
+            entity.HasOne(d => d.User).WithMany(p => p.Clubs).HasForeignKey(d => d.UserId);
+        });
+
+        modelBuilder.Entity<User>(entity =>
+        {
+            entity.HasKey(e => e.UserId).HasName("PK__User__1788CCACE278D849");
+
+            entity.ToTable("User");
+
+            entity.Property(e => e.UserId).HasColumnName("UserID");
+            entity.Property(e => e.Email)
+                .HasMaxLength(25)
+                .IsUnicode(false);
+            entity.Property(e => e.FirstName)
+                .HasMaxLength(25)
+                .IsUnicode(false);
+            entity.Property(e => e.LastName)
+                .HasMaxLength(25)
+                .IsUnicode(false);
+            entity.Property(e => e.MembershipStartDate)
+                .HasDefaultValueSql("(getdate())")
+                .HasColumnType("datetime");
+            entity.Property(e => e.MembershipStatus)
+                .HasMaxLength(10)
+                .IsUnicode(false);
+            entity.Property(e => e.Phone)
+                .HasMaxLength(15)
+                .IsUnicode(false);
+        });
+
+        OnModelCreatingPartial(modelBuilder);
+    }
+
+    partial void OnModelCreatingPartial(ModelBuilder modelBuilder);
+}
