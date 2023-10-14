7b3b656c3e5b1107aaba215257d577607ae42d53
mikrewongel <wongelmikre@gmail.com>
2023-10-14 17:33:57 -0400

    HTTP VERBS

---
+++ TMSBlazorAPI/Controllers/ClubsController.cs
@@ -0,0 +1,129 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using TMSBlazorAPI.Data;
+
+namespace TMSBlazorAPI.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class ClubsController : ControllerBase
+    {
+        private readonly TMSDbContext _context;
+
+        public ClubsController(TMSDbContext context)
+        {
+            _context = context;
+        }
+
+        // GET: api/Clubs
+        [HttpGet]
+        public async Task<ActionResult<IEnumerable<Club>>> GetClubs()
+        {
+          if (_context.Clubs == null)
+          {
+              return NotFound();
+          }
+            return Ok(await _context.Clubs.ToListAsync());
+        }
+
+        // GET: api/Clubs/5
+        [HttpGet("{id}")]
+        public async Task<ActionResult<Club>> GetClub(int id)
+        {
+          if (_context.Clubs == null)
+          {
+              return NotFound();
+          }
+            var club = await _context.Clubs.FindAsync(id);
+
+            if (club == null)
+            {
+                return NotFound();
+            }
+
+            return Ok(club);
+        }
+
+        // PUT: api/Clubs/5
+        // To protect from overposting attacks, see https://go.microsoft.com/fwlink/?linkid=2123754
+        [HttpPut("{id}")]
+        public async Task<IActionResult> PutClub(int id, Club club)
+        {
+            if (id != club.ClubId)
+            {
+                return BadRequest();
+            }
+
+            _context.Entry(club).State = EntityState.Modified;
+
+            try
+            {
+                await _context.SaveChangesAsync();
+            }
+            catch (DbUpdateConcurrencyException)
+            {
+                if (!await ClubExists(id))
+                {
+                    return NotFound();
+                }
+                else
+                {
+                    throw;
+                }
+            }
+
+            return NoContent();
+        }
+
+        // POST: api/Clubs
+        // To protect from overposting attacks, see https://go.microsoft.com/fwlink/?linkid=2123754
+        [HttpPost]
+        public async Task<ActionResult<Club>> PostClub(Club club)
+        {
+          if (_context.Clubs == null)
+          {
+              return Problem("Entity set 'TMSDbContext.Clubs'  is null.");
+          }
+            await _context.Clubs.AddAsync(club);
+            await _context.SaveChangesAsync();
+
+            return CreatedAtAction(nameof(GetClubs), new { id = club.ClubId }, club);
+        }
+
+        // DELETE: api/Clubs/5
+        [HttpDelete("{id}")]
+        public async Task<IActionResult> DeleteClub(int id)
+        {
+            if (_context.Clubs == null)
+            {
+                return NotFound();
+            }
+            var club = await _context.Clubs.FindAsync(id);
+            if (club == null)
+            {
+                return NotFound();
+            }
+
+            _context.Clubs.Remove(club);
+            await _context.SaveChangesAsync();
+
+            return NoContent();
+        }
+
+
+        private async Task<bool> ClubExists(int id)
+        {
+            return await (_context.Clubs?.AnyAsync(e => e.ClubId == id));
+        }
+
+        //private bool ClubExists(int id)
+        //{
+        //    return (_context.Clubs?.Any(e => e.ClubId == id)).GetValueOrDefault();
+        //}
+    }
+}
